9d8b39a3a42b1aa759a29d15ed9859ca6c3ceb31
SurajSarangi <surajsarangi1@gmail.com>
2021-04-22 20:49:21 +0530

    initialised the repository with all starter files

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://greyKnight:1234@node-ninja.6awgg.gcp.mongodb.net/node-auth';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(PORT, () => console.log(`Listening on port ${PORT}`)))
+  .catch((err) => console.log(err));
+
+app.use((req,res,next) => {
+  console.log(req.method, '\t', req.path);
+  next();
+})
+
+// routes
+app.get('/', (req, res) => res.render('home', { title: "Home "}));
+app.get('/games', (req, res) => res.render('games', { title: "Games" }));
