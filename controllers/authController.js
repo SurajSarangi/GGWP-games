d8756e4551a4cee0f66d416553038f6de8403b77
SurajSarangi <surajsarangi1@gmail.com>
2021-04-23 19:21:25 +0530

    added error message functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,23 @@
 const User = require('../models/User');
 
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    let errors = { email : '', password : '' };
+
+    if(err.code === 11000){
+        errors.email = "That email has already been registered. Try logging in"
+        return errors;
+    }
+
+    if(err.message.includes('user validation failed')){
+        Object.values(err.errors).forEach(({properties}) => {
+            errors[properties.path] = properties.message;
+        });
+    }
+
+    return errors;
+};
+
 const signup_get = (req,res) => {
     res.render('signup', { title: "Sign Up" });
 };
@@ -12,8 +30,8 @@ const signup_post = async (req,res) => {
         res.status(201).json(user);
     }
     catch(err){
-        console.log(err);
-        res.status(400).send('error, user not created');
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
     }
 };
 
