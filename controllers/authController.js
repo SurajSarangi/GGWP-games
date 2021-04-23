46170adb8c980e3864389795f61ec7a89b954056
SurajSarangi <surajsarangi1@gmail.com>
2021-04-23 18:39:29 +0530

    added mongoose to create a schema

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,29 @@
+const User = require('../models/User');
+
 const signup_get = (req,res) => {
     res.render('signup', { title: "Sign Up" });
 };
 
-const signup_post = (req,res) => {
-    res.send('new signup');
+const signup_post = async (req,res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.create({ email, password });
+        res.status(201).json(user);
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).send('error, user not created');
+    }
 };
 
 const login_get = (req,res) => {
     res.render('login', { title: "Log In" });
 };
 
-const login_post = (req,res) => {
-    res.send('user logged in');
+const login_post = async (req,res) => {
+    const { email, password } = req.body;
+    res.send(`${email} ${password}`);
 };
 module.exports = {
     signup_get,
